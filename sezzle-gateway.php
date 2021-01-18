45790a18f527cc5669e14efa84dc626ac8d5a49b
Surendhar Elamurugan <surendhar.elamurugan@sezzle.com>
2021-01-18 15:07:51 +0530

    added network enabled condition check for woocommerce

--- sezzle-gateway.php
+++ sezzle-gateway.php
@@ -23,7 +23,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+if ( in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins'))) || is_plugin_active_for_network( 'woocommerce/woocommerce.php' ) ) {
 
     add_action('plugins_loaded', 'woocommerce_sezzlepay_init');
 
